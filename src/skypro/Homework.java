6331b4abf5b36c859593493fe52a4f489dd79d85
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-15 00:10:10 +0300

    задание 2

--- src/skypro/Homework.java
+++ src/skypro/Homework.java
@@ -11,5 +11,18 @@ public class Homework {
         double sugar = 189.9991;
         char symbol = 21;
         boolean isWeightFructs = 10 > cherry;
+        // Задание 2
+        float weightFirstBoxer = 78.2f;
+        float weightSecondBoxer = 82.7f;
+        float totalWeight = weightFirstBoxer + weightSecondBoxer;
+        System.out.println("Общий вес бойцов " + totalWeight + "кг");
+        float differenceBetween = weightSecondBoxer - weightFirstBoxer;
+        /*
+        Пытался пойти сложным путем как в обучающем ролике
+        вес первого бойца минус второго, потом через переменную overload,
+        но что-то пошло не так. Скорее всего, здесь это нельзя применить т.к.
+        нет дополнительных условий.
+         */
+        System.out.println("Разница в весе между бойцами " + differenceBetween + "кг");
     }
 }
