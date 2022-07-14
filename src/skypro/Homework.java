a56f6c6b2a355dfa8048699520658bb53e8be832
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-15 01:15:23 +0300

    задание 3

--- src/skypro/Homework.java
+++ src/skypro/Homework.java
@@ -3,6 +3,7 @@ package skypro;
 public class Homework {
     public static void main(String[] args) {
     // Задание 1
+
         byte cherry = 120;
         short apples = 25321;
         int kiwi = 158769;
@@ -11,7 +12,9 @@ public class Homework {
         double sugar = 189.9991;
         char symbol = 21;
         boolean isWeightFructs = 10 > cherry;
+
         // Задание 2
+
         float weightFirstBoxer = 78.2f;
         float weightSecondBoxer = 82.7f;
         float totalWeight = weightFirstBoxer + weightSecondBoxer;
@@ -24,5 +27,15 @@ public class Homework {
         нет дополнительных условий.
          */
         System.out.println("Разница в весе между бойцами " + differenceBetween + "кг");
+
+        // Задание 3
+
+        short bananas = 80;
+        double milk = 1.05;
+        short iceCream = 100;
+        short egg = 70;
+        double weight = bananas * 5 + milk * 200 + iceCream * 2 + egg * 4;
+        System.out.println("Вес завтрака составляет " + weight + " грамм"); // не смог форматировать вывод в целое число
+        System.out.println("вес завтрака составляет " + weight / 1000 + " килограмм");
     }
 }
