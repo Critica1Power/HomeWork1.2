c64a973697405c66490daf9021b14011974f0dc3
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-14 23:44:22 +0300

    задание 1

--- src/skypro/Homework.java
+++ src/skypro/Homework.java
@@ -2,6 +2,14 @@ package skypro;
 
 public class Homework {
     public static void main(String[] args) {
-
+    // Задание 1
+        byte cherry = 120;
+        short apples = 25321;
+        int kiwi = 158769;
+        long skateBoard = 3698521L;
+        float salt = 15.850f;
+        double sugar = 189.9991;
+        char symbol = 21;
+        boolean isWeightFructs = 10 > cherry;
     }
 }
